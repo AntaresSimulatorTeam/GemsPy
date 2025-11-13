e3ddbe907beac936ee2c2cf25cc7360cf768c068
dusanparipovic <dusan.paripovic@rte-international.com>
2025-11-13 13:56:20 +0100

    fix(black format): add black format for parsing.py

--- src/gems/pypsa_converter/parsing.py
+++ src/gems/pypsa_converter/parsing.py
@@ -21,6 +21,7 @@ from yaml import safe_load
 
 from gems.pypsa_converter.models.gems_system_yaml_schema import GemsSystem
 
+
 def parse_yaml_components(input_study: TextIO) -> GemsSystem:
     tree = safe_load(input_study)
     return GemsSystem.model_validate(tree["system"])
